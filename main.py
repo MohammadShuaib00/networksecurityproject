9e1470286ffff66e5d2299ab24446d3517c67e65
Mohammad Shuaib <mohammadshuaib3455@gmail.com>
2024-10-29 09:36:09 +0530

    Model Trained

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logging import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+)
 from networksecurity.pipeline.training_pipeline import *
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-import os,sys
+import os, sys
 
 
 if __name__ == "__main__":
@@ -23,6 +28,16 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation = DataTransformation(
+            data_validation_artifact, data_transformation_config
+        )
+        data_transformation_artifact = (
+            data_transformation.initiate_data_transformation()
+        )
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys.exc_info())
